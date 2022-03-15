5651c2ee224fe35932aa0fd4117035a7d933236e
IDKWNTCMF <71064019+IDKWNTCMF@users.noreply.github.com>
2022-03-16 00:03:11 +0300

    Update Main.kt

--- Exercise2/Main.kt
+++ Exercise2/Main.kt
@@ -1,20 +1,27 @@
 import java.io.File
 
+class FileFormatException : Exception("Wrong file format!")
+
 fun main() {
-    val input = File("tmp.txt").readLines()
-    val parsedInput: List<List<String>> = input.map { str -> str.split(" ", "\t").filter { it.isNotEmpty() } }
-    val refs: MutableMap<String, String> = mutableMapOf()
-    parsedInput.forEach { lst ->
-        if (lst.size >= 4 && lst[0].isNotEmpty() && lst[0][0] == '#') {
-            refs[lst[0]] = lst[3]
+    try {
+        val input = File("tmp.txt").readLines()
+        val parsedInput: List<List<String>> = input.map { str -> str.split(" ", "\t").filter { it.isNotEmpty() } }
+        val refs: MutableMap<String, String> = mutableMapOf()
+        parsedInput.forEach { lst ->
+            if (lst.size >= 4 && lst[0].isNotEmpty() && lst[0][0] == '#') {
+                refs[lst[0]] = lst[3]
+            }
         }
-    }
-    parsedInput.forEach { lst ->
-        if (lst.size >= 3 && lst[1] == "invokevirtual") {
-            val refToClass = refs[lst[2]]?.split(".")?.get(0) ?: return
-            val refToClassName = refs[refToClass] ?: return
-            val className = refs[refToClassName] ?: return
-            println(className)
+        parsedInput.forEach { println(it) }
+        parsedInput.forEach { lst ->
+            if (lst.size >= 3 && lst[1] == "invokevirtual") {
+                val refToClass = refs[lst[2]]?.split(".")?.get(0) ?: throw FileFormatException()
+                val refToClassName = refs[refToClass] ?: throw FileFormatException()
+                val className = refs[refToClassName] ?: throw FileFormatException()
+                println(className)
+            }
         }
+    } catch (e : FileFormatException) {
+        println(e)
     }
 }
